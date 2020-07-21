e3477d791b68f77d94211be49fd653ed37473df4
Matthew VH <vanheckematt@gmail.com>
2020-07-21 14:14:27 -0500

    Moved the 'allNominations' object, and the associated methods which pull the info from the API and sorts it.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Axios from 'axios';
 import ExploreNominations from './ExploreNominations';
+import { groupByCriteria, subGroupByCriteria } from './helperFunctions';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App container">
-      <div className="row">
-        <ExploreNominations />
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {allNominations: {}};
+    this.getAllNominations = this.getAllNominations.bind(this);
+  }
+  componentDidMount(){
+    this.getAllNominations();
+  }
+  async getAllNominations(){
+    let response = await Axios.get("https://localhost:44366/api/Nominations/");
+    let sortedResponseData = subGroupByCriteria(groupByCriteria(response.data, "year"), "awardCategory");
+    console.log(sortedResponseData);
+    this.setState({allNominations: sortedResponseData});
+  }
+  render() {
+    return (
+      <div className="App container">
+        <div className="row">
+          <ExploreNominations />
+        </div>
+  
       </div>
-
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/ExploreNominations.js
+++ src/ExploreNominations.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
-import { groupByCriteria, subGroupByCriteria } from './helperFunctions';
-import Axios from 'axios';
 
 class ExploreNominations extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            allNominations: {}
-        }
-        this.getAllNominations = this.getAllNominations.bind(this);
-    }
-    componentDidMount(){
-        this.getAllNominations();
+        this.state = {};
     }
     render(){
         return(
             <h1>Explore Nominations</h1>
         )
-    }
-    async getAllNominations(){
-        let response = await Axios.get("https://localhost:44366/api/Nominations/");
-        let sortedResponseData = subGroupByCriteria(groupByCriteria(response.data, "year"), "awardCategory");
-        console.log(sortedResponseData);
-        this.setState({allNominations: sortedResponseData});
     }
 }
 
